baa962034c1ae2103d8889940cab8a58b9617ca0
ruizhang <kwf8hq@virginia.edu>
2024-03-03 02:41:06 -0500

    models/db

--- store/models.py
+++ store/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Marker(models.Model):
-    title = models.CharField(max_length=100)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    address = models.TextField(max_length=200)
+    mapID = models.TextField(max_length=200)
+    user = models.TextField(max_length=200)
