9d70c7be636414aca641cf8fdbc927cdfa5fc0ca
ruizhang <kwf8hq@virginia.edu>
2024-03-03 02:32:19 -0500

    adddb start

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Marker(models.Model):
+    title = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
