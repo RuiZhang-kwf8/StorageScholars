496d59eb5e92a84511c1268afde8b5aa3dc30746
ruizhang <kwf8hq@virginia.edu>
2024-03-03 03:02:19 -0500

    postgres

--- ruiiiX/settings.py
+++ ruiiiX/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'ruiiiX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storagescholars1',
+        'USER': 'postgres',
+        'PASSWORD': '039210',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
